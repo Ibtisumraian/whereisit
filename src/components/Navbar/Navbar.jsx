64da35f01d8bc1f5a374a29f7b48d933a479a701
Ibtisumraian <ibtisumraian@gmail.com>
2025-06-04 15:40:29 +0600

    updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,21 +4,37 @@ import { NavLink } from "react-router";
 const Navbar = () => {
   return (
     <div>
-      <div className="flex items-center justify-between py-7 w-11/12 mx-auto">
-        <div>
-          <h1 className="text-4xl fontInter text-[#00A79D] font-bold">WhereIsIt</h1>
-        </div>
-        <div className="flex items-center gap-4">
-          <div className="flex items-center gap-7 text-base font-semibold">
+      <div className="navbar w-11/12 mx-auto py-9 ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <div className="flex flex-col items-center gap-3 text-base font-semibold">
             <button className="">Home</button>
             <button className="">Sign In</button>
             <button className="">Sign Out</button>
           
           </div>
-
-         
-         
-                  <div className="dropdown dropdown-end">
+      </ul>
+    </div>
+    <a className=" hidden md:block text-4xl fontInter font-bold text-[#00A79D]">WhereIsIt</a>
+  </div>
+  <div className="navbar-end flex gap-7">
+    <div className=" hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <div className="flex items-center gap-7 text-base font-semibold">
+            <button className="">Home</button>
+            <button className="">Sign In</button>
+            <button className="">Sign Out</button>
+          
+          </div>
+    </ul>
+  </div>
+    <div className=" dropdown dropdown-end">
                       <div tabIndex={0} role="button" className="">
                           <div className="relative flex flex-col justify-center">
                             <div className="avatar myDIV">
@@ -46,9 +62,12 @@ const Navbar = () => {
                             <h1>Sign Out</h1>
                         </div>
                     </div>
-                    </div>
-        </div>
-      </div>
+    </div>
+  </div>
+  {/* <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div> */}
+</div>
     </div>
   );
 };

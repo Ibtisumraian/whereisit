bd673bb48303235ef5ec903cae7250d67f8da3fc
Ibtisumraian <ibtisumraian@gmail.com>
2025-06-04 13:56:54 +0600

    added static Latest Lost & Found Items section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,105 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import { FaCalendarDays } from 'react-icons/fa6';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <div>
+                    <div className='flex items-center justify-center text-4xl fontInter font-semibold mb-12 mt-32'>
+                        <h1>Latest Lost & Found Items</h1>
+                    </div>
+                <div className='grid grid-cols-3 gap-6 w-9/12 mx-auto'>
+                    <div className='bg-gray-50 shadow-md '>
+                    <div className='flex justify-center items-center rounded-lg'>
+                        <img className='w-full h-[300px] rounded-t-lg' src="https://res.cloudinary.com/dd4np04jl/image/upload/v1749016006/a0d3dbd5fd1d628f9177cf0a51423603_udyeig.jpg" alt="" />
+                    </div>
+                        <div className='flex flex-col gap-4 p-6'>
+                            <h5 className='bg-[#00A79D20] py-1 px-3 w-fit rounded-xl text-[#00A79D]'>Lost</h5>
+                            <h1 className='fontInter font-bold text-base'>Lost Blue Backpack</h1>
+                            <h2 className='flex items-center gap-3'> <FaCalendarDays className='text-[#00A79D]' /> 24-5-2025</h2>
+                            <p>Contains a laptop and textbooks. <br /> Last seen near platform 3.</p>
+                            <button className='btn w-fit'>View Details</button>
+                    </div>
+                    </div>
+
+                    <div className='bg-gray-50 shadow-md '>
+                    <div className='flex justify-center items-center rounded-lg'>
+                        <img className='w-full h-[300px] rounded-t-lg' src="https://res.cloudinary.com/dd4np04jl/image/upload/v1749016006/a0d3dbd5fd1d628f9177cf0a51423603_udyeig.jpg" alt="" />
+                    </div>
+                        <div className='flex flex-col gap-4 p-6'>
+                            <h5 className='bg-[#00A79D20] py-1 px-3 w-fit rounded-xl text-[#00A79D]'>Lost</h5>
+                            <h1 className='fontInter font-bold text-base'>Lost Blue Backpack</h1>
+                            <h2 className='flex items-center gap-3'> <FaCalendarDays className='text-[#00A79D]' /> 24-5-2025</h2>
+                            <p>Contains a laptop and textbooks. <br /> Last seen near platform 3.</p>
+                            <button className='btn w-fit'>View Details</button>
+                    </div>
+                    </div>
+
+                    
+
+                    <div className='bg-gray-50 shadow-md '>
+                    <div className='flex justify-center items-center rounded-lg'>
+                        <img className='w-full h-[300px] rounded-t-lg' src="https://res.cloudinary.com/dd4np04jl/image/upload/v1749016006/a0d3dbd5fd1d628f9177cf0a51423603_udyeig.jpg" alt="" />
+                    </div>
+                        <div className='flex flex-col gap-4 p-6'>
+                            <h5 className='bg-[#00A79D20] py-1 px-3 w-fit rounded-xl text-[#00A79D]'>Lost</h5>
+                            <h1 className='fontInter font-bold text-base'>Lost Blue Backpack</h1>
+                            <h2 className='flex items-center gap-3'> <FaCalendarDays className='text-[#00A79D]' /> 24-5-2025</h2>
+                            <p>Contains a laptop and textbooks. <br /> Last seen near platform 3.</p>
+                            <button className='btn w-fit'>View Details</button>
+                    </div>
+                    </div>
+
+                    
+
+                    <div className='bg-gray-50 shadow-md '>
+                    <div className='flex justify-center items-center rounded-lg'>
+                        <img className='w-full h-[300px] rounded-t-lg' src="https://res.cloudinary.com/dd4np04jl/image/upload/v1749016006/a0d3dbd5fd1d628f9177cf0a51423603_udyeig.jpg" alt="" />
+                    </div>
+                        <div className='flex flex-col gap-4 p-6'>
+                            <h5 className='bg-[#00A79D20] py-1 px-3 w-fit rounded-xl text-[#00A79D]'>Lost</h5>
+                            <h1 className='fontInter font-bold text-base'>Lost Blue Backpack</h1>
+                            <h2 className='flex items-center gap-3'> <FaCalendarDays className='text-[#00A79D]' /> 24-5-2025</h2>
+                            <p>Contains a laptop and textbooks. <br /> Last seen near platform 3.</p>
+                            <button className='btn w-fit'>View Details</button>
+                    </div>
+                    </div>
+
+                    
+
+                    <div className='bg-gray-50 shadow-md '>
+                    <div className='flex justify-center items-center rounded-lg'>
+                        <img className='w-full h-[300px] rounded-t-lg' src="https://res.cloudinary.com/dd4np04jl/image/upload/v1749016006/a0d3dbd5fd1d628f9177cf0a51423603_udyeig.jpg" alt="" />
+                    </div>
+                        <div className='flex flex-col gap-4 p-6'>
+                            <h5 className='bg-[#00A79D20] py-1 px-3 w-fit rounded-xl text-[#00A79D]'>Lost</h5>
+                            <h1 className='fontInter font-bold text-base'>Lost Blue Backpack</h1>
+                            <h2 className='flex items-center gap-3'> <FaCalendarDays className='text-[#00A79D]' /> 24-5-2025</h2>
+                            <p>Contains a laptop and textbooks. <br /> Last seen near platform 3.</p>
+                            <button className='btn w-fit'>View Details</button>
+                    </div>
+                    </div>
+
+                    
+
+                    <div className='bg-gray-50 shadow-md '>
+                    <div className='flex justify-center items-center rounded-lg'>
+                        <img className='w-full h-[300px] rounded-t-lg' src="https://res.cloudinary.com/dd4np04jl/image/upload/v1749016006/a0d3dbd5fd1d628f9177cf0a51423603_udyeig.jpg" alt="" />
+                    </div>
+                        <div className='flex flex-col gap-4 p-6'>
+                            <h5 className='bg-[#00A79D20] py-1 px-3 w-fit rounded-xl text-[#00A79D]'>Lost</h5>
+                            <h1 className='fontInter font-bold text-base'>Lost Blue Backpack</h1>
+                            <h2 className='flex items-center gap-3'> <FaCalendarDays className='text-[#00A79D]' /> 24-5-2025</h2>
+                            <p>Contains a laptop and textbooks. <br /> Last seen near platform 3.</p>
+                            <button className='btn w-fit'>View Details</button>
+                    </div>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
     );
 };

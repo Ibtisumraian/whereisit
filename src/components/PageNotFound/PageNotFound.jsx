885464b0f485fbf969df4bd8c076821b57787fce
Ibtisumraian <ibtisumraian@gmail.com>
2025-06-17 21:36:18 +0600

    responsive page not found added

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -11,7 +11,7 @@ const PageNotFound = () => {
         <div>
             <div className=''>
                 <div className='flex justify-center items-center mt-18 drop-shadow-xl/800'>
-                    <div className=' w-6/12 mx-auto relative bg-[#56c9c199] rounded-2xl '> 
+                    <div className='w-10/12 md:w-9/12 lg:w-8/12 2xl:w-6/12 mx-auto relative bg-[#56c9c199] rounded-2xl '> 
                         <div className='w-fit p-6 absolute'>
                             <Link to='/'>
                         <motion.button
@@ -23,14 +23,14 @@ const PageNotFound = () => {
                         </Link>
                         </div>
                         <div className='bg-base-300 text-center rounded-tl-full  flex flex-col  justify-between items-center gap-8 p-8'>
-                               <div>
+                        <div className='mt-8 hidden sm:block'>
                             <Lottie animationData={pageNotFoundLottie} loop={true} className='w-[300px]'></Lottie>
                         </div>
-                    <div className='flex flex-col justify-center items-center gap-6'>
+                    <div className='flex flex-col justify-center items-center gap-6 mt-16 sm:mt-0'>
                         <h1 className='text-3xl sm:text-7xl font-bold text-gray-600 '>404</h1>
                         <h1 className='text-3xl sm:text-5xl font-bold text-gray-600 '>Page Not Found!!</h1>
                         <h1 className=' text-gray-600 text-base sm:text-xl'>Oops! The page you're looking for doesn't exist.</h1>
-                        <h1 className='text-gray-600 text-sm sm:text-base  flex justify-center items-center gap-2'><FaRegistered /> There is no page on the path name : { location.pathname}</h1>
+                        <h1 className='text-gray-600 text-sm sm:text-base  flex justify-center items-center gap-2'> There is no page on the path name : { location.pathname}</h1>
                         
                     </div>
                      </div>
